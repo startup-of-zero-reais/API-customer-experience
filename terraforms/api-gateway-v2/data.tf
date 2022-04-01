63be7cbf888934a69e9e8d9a8333cb896ff2a0dd
Jean <jean.carlosmolossi@gmail.com>
2022-03-31 22:09:00 -0300

    fix: comment uncreated functions

--- terraforms/api-gateway-v2/data.tf
+++ terraforms/api-gateway-v2/data.tf
@@ -2,21 +2,21 @@ data "aws_apigatewayv2_api" "this" {
   api_id = var.api_id
 }
 
-data "aws_lambda_function" "company" {
-  function_name = format("%s-%s", var.project_name, "company")
-}
+# data "aws_lambda_function" "company" {
+#   function_name = format("%s-%s", var.project_name, "company")
+# }
 
-data "aws_lambda_function" "favorites" {
-  function_name = format("%s-%s", var.project_name, "favorites")
-}
+# data "aws_lambda_function" "favorites" {
+#   function_name = format("%s-%s", var.project_name, "favorites")
+# }
 
-data "aws_lambda_function" "orders" {
-  function_name = format("%s-%s", var.project_name, "orders")
-}
+# data "aws_lambda_function" "orders" {
+#   function_name = format("%s-%s", var.project_name, "orders")
+# }
 
-data "aws_lambda_function" "session" {
-  function_name = format("%s-%s", var.project_name, "session")
-}
+# data "aws_lambda_function" "session" {
+#   function_name = format("%s-%s", var.project_name, "session")
+# }
 
 data "aws_lambda_function" "user" {
   function_name = format("%s-%s", var.project_name, "user")
