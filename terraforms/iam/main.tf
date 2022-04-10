495921fc254ec3e473a60036feb1acddbfc8228e
Jean <jean.carlosmolossi@gmail.com>
2022-04-10 11:44:55 -0300

    chore: all tables permission

--- terraforms/iam/main.tf
+++ terraforms/iam/main.tf
@@ -82,7 +82,7 @@ resource "aws_iam_policy" "lambda_db" {
       "Action": [
         "dynamodb:*"
       ],
-      "Resource": "arn:aws:dynamodb:us-east-1:599981017555:table/*",
+      "Resource": "arn:aws:dynamodb:us-east-1:599981017555:*",
       "Effect": "Allow"
     }
   ]
